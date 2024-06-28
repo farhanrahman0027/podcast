1aa0e49bf792920baf597ff40b6055b434e7a2a0
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-28 14:21:04 -0600

    feat(PodcastCard): implement the card image display

--- components/shared/PodcastCard.tsx
+++ components/shared/PodcastCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const PodcastCard = ({
   imgUrl,
   title,
@@ -9,7 +11,13 @@ const PodcastCard = ({
   description: string;
   podcastId: number;
 }) => {
-  return <div>PodcastCard</div>;
+  return (
+    <div className="cursor-pointer">
+      <figure className="flex flex-col gap-2">
+        <Image src={imgUrl} alt={title} width={174} height={174} />
+      </figure>
+    </div>
+  );
 };
 
 export default PodcastCard;
