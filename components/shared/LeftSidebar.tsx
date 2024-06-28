ff28fe36d019c1b8c81cc8aaa4fac309227af8c9
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-28 13:11:40 -0600

    feat(LeftSidebar): implement the left sidebar route links along as all of their properties and styles when active route

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
@@ -14,6 +22,25 @@ const LeftSidebar = () => {
             Podcastr
           </h1>
         </Link>
+
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+
+          return (
+            <Link
+              key={label}
+              href={route}
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+              )}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p className="">{label}</p>
+            </Link>
+          );
+        })}
       </nav>
     </section>
   );
