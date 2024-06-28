f80271820056d1ac4759e65ad6eaff0b5b083548
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-28 14:23:20 -0600

    feat(PodcastCard): implement the complete PodcastCard component

--- components/shared/PodcastCard.tsx
+++ components/shared/PodcastCard.tsx
@@ -15,6 +15,13 @@ const PodcastCard = ({
     <div className="cursor-pointer">
       <figure className="flex flex-col gap-2">
         <Image src={imgUrl} alt={title} width={174} height={174} />
+
+        <div className="flex flex-col">
+          <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
+          <h2 className="text-12 truncate font-normal capitalize text-white-4">
+            {description}
+          </h2>
+        </div>
       </figure>
     </div>
   );
