baeb53a18474451eae2df50696dba16b50ef44bb
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-28 15:11:08 -0600

    feat(convex/users.ts): implement the complete createUser convex function

--- convex/users.ts
+++ convex/users.ts
@@ -48,22 +48,22 @@ export const getTopUserByPodcastCount = query({
   },
 });
 
-// export const createUser = internalMutation({
-//   args: {
-//     clerkId: v.string(),
-//     email: v.string(),
-//     imageUrl: v.string(),
-//     name: v.string(),
-//   },
-//   handler: async (ctx, args) => {
-//     await ctx.db.insert("users", {
-//       clerkId: args.clerkId,
-//       email: args.email,
-//       imageUrl: args.imageUrl,
-//       name: args.name,
-//     });
-//   },
-// });
+export const createUser = internalMutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    imageUrl: v.string(),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("users", {
+      clerkId: args.clerkId,
+      email: args.email,
+      imageUrl: args.imageUrl,
+      name: args.name,
+    });
+  },
+});
 
 // export const updateUser = internalMutation({
 //   args: {
