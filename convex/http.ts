1346836502a072f5c096b432d39815c39fa0e257
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-29 23:53:50 -0600

    feat(convex/https.ts): implement the complete validateRequest function

--- convex/http.ts
+++ convex/http.ts
@@ -49,23 +49,23 @@ http.route({
   handler: handleClerkWebhook,
 });
 
-// const validateRequest = async (
-//   req: Request
-// ): Promise<WebhookEvent | undefined> => {
-//   const webhookSecret = process.env.CLERK_WEBHOOK_SECRET!;
-//   if (!webhookSecret) {
-//     throw new Error("CLERK_WEBHOOK_SECRET is not defined");
-//   }
-//   const payloadString = await req.text();
-//   const headerPayload = req.headers;
-//   const svixHeaders = {
-//     "svix-id": headerPayload.get("svix-id")!,
-//     "svix-timestamp": headerPayload.get("svix-timestamp")!,
-//     "svix-signature": headerPayload.get("svix-signature")!,
-//   };
-//   const wh = new Webhook(webhookSecret);
-//   const event = wh.verify(payloadString, svixHeaders);
-//   return event as unknown as WebhookEvent;
-// };
+const validateRequest = async (
+  req: Request
+): Promise<WebhookEvent | undefined> => {
+  const webhookSecret = process.env.CLERK_WEBHOOK_SECRET!;
+  if (!webhookSecret) {
+    throw new Error("CLERK_WEBHOOK_SECRET is not defined");
+  }
+  const payloadString = await req.text();
+  const headerPayload = req.headers;
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id")!,
+    "svix-timestamp": headerPayload.get("svix-timestamp")!,
+    "svix-signature": headerPayload.get("svix-signature")!,
+  };
+  const wh = new Webhook(webhookSecret);
+  const event = wh.verify(payloadString, svixHeaders);
+  return event as unknown as WebhookEvent;
+};
 
 export default http;
