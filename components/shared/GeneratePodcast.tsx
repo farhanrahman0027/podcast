db4209473313876626529ec2e84cf9c8f4d070ce
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-30 14:14:49 -0600

    feat(GenereatePodcast): implement the component label

--- components/shared/GeneratePodcast.tsx
+++ components/shared/GeneratePodcast.tsx
@@ -1,5 +1,24 @@
-const GeneratePodcast = () => {
-  return <div>GeneratePodcast</div>;
+import { GeneratePodcastProps } from "@/types";
+import { Label } from "../ui/label";
+
+const GeneratePodcast = ({
+  setAudioStorageId,
+  setAudio,
+  voiceType,
+  audio,
+  voicePrompt,
+  setVoicePrompt,
+  setAudioDuration,
+}: GeneratePodcastProps) => {
+  return (
+    <div>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
+          Ai Prompt to generate Podcast
+        </Label>
+      </div>
+    </div>
+  );
 };
 
 export default GeneratePodcast;
