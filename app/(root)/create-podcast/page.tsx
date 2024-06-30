5c1c75fe4281a8f2dd541b496c41185b75d74d09
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-30 09:57:42 -0600

    implement the shadcn Select component

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -65,6 +66,12 @@ const CreatePodcast = () => {
                 </FormItem>
               )}
             />
+
+            <div className="flex flex-col gap-2.5">
+              <Label className="text-16 font-bold text-white-1">
+                Select AI Voice
+              </Label>
+            </div>
           </div>
         </form>
       </Form>
