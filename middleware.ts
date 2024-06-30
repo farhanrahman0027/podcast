ffb5e81abf9679151750669288919963c14f82ae
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-30 09:31:56 -0600

    feat(ConvexClerkProvider): implement the complete component along as the sign-in and sign-up clerk authentication pages

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoutes = createRouteMatcher(["/sign-in", "/sign-up", "/"]);
+const isPublicRoutes = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoutes(req)) auth().protect();
