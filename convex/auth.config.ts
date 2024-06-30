95fb25353c2606a847725053b26a5a7d950ac18f
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-30 01:00:22 -0600

    implement the complete middleware.ts file

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://kind-arachnid-71.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
