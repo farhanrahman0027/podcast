b21c02a30de306369f6485495af05341d89170f1
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-01 14:46:25 -0600

    feat(convex/openai.ts): implement the complete generateThumbnailAction function

--- convex/openai.ts
+++ convex/openai.ts
@@ -1,8 +1,8 @@
 import { action } from "./_generated/server";
 import { v } from "convex/values";
 
-import OpenAI from "openai";
 import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
+import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
