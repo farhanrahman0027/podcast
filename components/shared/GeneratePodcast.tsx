45bb289aa5ad3410e27cba53b4d17976c20e8caf
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-01 14:44:12 -0600

    install the openai library dependencies

--- components/shared/GeneratePodcast.tsx
+++ components/shared/GeneratePodcast.tsx
@@ -7,12 +7,38 @@ import { Button } from "../ui/button";
 import { Loader } from "lucide-react";
 import { useState } from "react";
 
-const useGeneratePodcast = (props: GeneratePodcastProps) => {
-  // Logic for podcast generation
+const useGeneratePodcast = ({
+  setAudio,
+  voiceType,
+  voicePrompt,
+  setAudioStorageId,
+}: GeneratePodcastProps) => {
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const generatePodcast = async () => {
+    setIsGenerating(true);
+    setAudio("");
+
+    if (!voicePrompt) {
+      // todo: show error message
+      return setIsGenerating(false);
+    }
+
+    try {
+      // const response = await getPodcastAudio({
+      //   voice: voiceType,
+      //   input: voicePrompt,
+      // });
+    } catch (error) {
+      console.log("Error generating podcast", error);
+      // todo: show error message
+      setIsGenerating(false);
+    }
+  };
 
   return {
-    isGenerating: false,
-    generatePodcast: () => {},
+    isGenerating,
+    generatePodcast,
   };
 };
 
