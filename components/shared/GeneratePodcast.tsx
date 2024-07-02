642f52f20b86c2f61ff9750efbcdb7d15457de67
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-02 09:35:11 -0600

    feat(GeneratePodcast): implement the useGeneratePodcast local custom hook complete logic

--- components/shared/GeneratePodcast.tsx
+++ components/shared/GeneratePodcast.tsx
@@ -6,9 +6,12 @@ import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { Loader } from "lucide-react";
 import { useState } from "react";
-import { useAction } from "convex/react";
+import { useAction, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { v4 as uuidv4 } from "uuid";
+import { generateUploadUrl } from "@/convex/files";
+
+import { useUploadFiles } from "@xixixao/uploadstuff/react";
 
 const useGeneratePodcast = ({
   setAudio,
@@ -18,7 +21,12 @@ const useGeneratePodcast = ({
 }: GeneratePodcastProps) => {
   const [isGenerating, setIsGenerating] = useState(false);
 
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const { startUpload } = useUploadFiles(generateUploadUrl);
+
   const getPodcastAudio = useAction(api.openai.generateAudioAction);
+
+  const getAudioUrl = useMutation(api.podcasts.getUrl);
 
   const generatePodcast = async () => {
     setIsGenerating(true);
@@ -36,8 +44,19 @@ const useGeneratePodcast = ({
       });
 
       const blob = new Blob([response], { type: "audio/mpeg" });
-      const filename = `podcast-${uuidv4()}.mp3`;
-      const file = new File([blob], filename, { type: "audio/mpeg" });
+      const fileName = `podcast-${uuidv4()}.mp3`;
+      const file = new File([blob], fileName, { type: "audio/mpeg" });
+
+      const uploaded = await startUpload([file]);
+      const storageId = (uploaded[0].response as any).storageId;
+
+      setAudioStorageId(storageId);
+
+      const audioUrl = await getAudioUrl({ storageId });
+      setAudio(audioUrl!);
+
+      setIsGenerating(false);
+      // todo: show success message
     } catch (error) {
       console.log("Error generating podcast", error);
       // todo: show error message
