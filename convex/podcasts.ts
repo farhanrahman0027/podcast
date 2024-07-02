2c072c735f1c7671af7dd250d6a843901c89fbe9
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-02 09:31:58 -0600

    feat(convex/podcasts.ts): implement the complete deletePodcast mutation function

--- convex/podcasts.ts
+++ convex/podcasts.ts
@@ -187,21 +187,21 @@ export const updatePodcastViews = mutation({
 });
 
 // this mutation will delete the podcast.
-// export const deletePodcast = mutation({
-//   args: {
-//     podcastId: v.id("podcasts"),
-//     imageStorageId: v.id("_storage"),
-//     audioStorageId: v.id("_storage"),
-//   },
-//   handler: async (ctx, args) => {
-//     const podcast = await ctx.db.get(args.podcastId);
+export const deletePodcast = mutation({
+  args: {
+    podcastId: v.id("podcasts"),
+    imageStorageId: v.id("_storage"),
+    audioStorageId: v.id("_storage"),
+  },
+  handler: async (ctx, args) => {
+    const podcast = await ctx.db.get(args.podcastId);
 
-//     if (!podcast) {
-//       throw new ConvexError("Podcast not found");
-//     }
+    if (!podcast) {
+      throw new ConvexError("Podcast not found");
+    }
 
-//     await ctx.storage.delete(args.imageStorageId);
-//     await ctx.storage.delete(args.audioStorageId);
-//     return await ctx.db.delete(args.podcastId);
-//   },
-// });
+    await ctx.storage.delete(args.imageStorageId);
+    await ctx.storage.delete(args.audioStorageId);
+    return await ctx.db.delete(args.podcastId);
+  },
+});
