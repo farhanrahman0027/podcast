302e87df915a63936ec778c4cd42862f4d964a8e
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-02 22:18:35 -0600

    feat(GenerateThumbnail): implement the last layout styles

--- components/shared/GenerateThumbnail.tsx
+++ components/shared/GenerateThumbnail.tsx
@@ -41,21 +41,21 @@ const GenerateThumbnail = ({
       </div>
 
       {isAiThumbnail ? (
-        <div>
-          <div className="flex flex-col gap-2.5">
+        <div className="flex flex-col gap-5">
+          <div className="mt-5 flex flex-col gap-2.5">
             <Label className="text-16 font-bold text-white-1">
-              Ai Prompt to generate Podcast
+              Ai Prompt to generate Thumbnail
             </Label>
             <Textarea
               className="input-class font-light focus-visible:ring-offset-orange-1"
-              placeholder="Provide text to generate audio"
+              placeholder="Provide text to generate thumbnail"
               rows={5}
               value={imagePrompt}
               onChange={(e) => setImagePrompt(e.target.value)}
             />
           </div>
 
-          <div className="mt-5 w-full max-w-[200px]">
+          <div className="w-full max-w-[200px]">
             <Button
               type="submit"
               className="text-16 w- bg-orange-1 py-4 font-bold text-white-1"
