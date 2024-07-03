23579bcfb72e52d33e497397c5a37954fd426094
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-02 21:58:55 -0600

    feat(GenerateThumbnail): implement the generate thumbnail button

--- components/shared/GenerateThumbnail.tsx
+++ components/shared/GenerateThumbnail.tsx
@@ -1,7 +1,13 @@
+import { Button } from "../ui/button";
+
 const GenerateThumbnail = () => {
   return (
     <>
-      <div className="generate_thumbnail"></div>
+      <div className="generate_thumbnail">
+        <Button type="button" variant="plain" className="bg-black-6">
+          Use AI to generate thumbnail
+        </Button>
+      </div>
     </>
   );
 };
