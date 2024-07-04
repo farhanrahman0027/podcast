c1a8e838991f3ceb41afa8853db2f6d925d124ad
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-04 12:34:48 -0600

    feat(PodcastDetails): implement the podcast database fetching along as the podcast views display

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
 import Image from "next/image";
 
 const PodcastDetails = ({
   params: { podcastId },
 }: {
-  params: { podcastId: string };
+  params: { podcastId: Id<"podcasts"> };
 }) => {
+  const podcast = useQuery(api.podcasts.getPodcastById, { podcastId });
+
   return (
     <section className="flex w-full flex-col">
       <header className="mt-9 flex items-center justify-between">
@@ -16,6 +23,7 @@ const PodcastDetails = ({
             width={24}
             height={24}
           />
+          <h2 className="text-16 font-bold text-white-1">{podcast?.views}</h2>
         </figure>
       </header>
     </section>
