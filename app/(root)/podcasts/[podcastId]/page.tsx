b34eebd147b8a1268b17f108973443c30db6657b
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-04 12:30:49 -0600

    feat(PodcastDetails): implement the headphone svg to the components header

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const PodcastDetails = ({
   params: { podcastId },
 }: {
@@ -7,6 +9,14 @@ const PodcastDetails = ({
     <section className="flex w-full flex-col">
       <header className="mt-9 flex items-center justify-between">
         <h1 className="text-20 font-bold text-white-1">Currently Playing</h1>
+        <figure className="flex gap-3">
+          <Image
+            src="/icons/headphone.svg"
+            alt="headphone"
+            width={24}
+            height={24}
+          />
+        </figure>
       </header>
     </section>
   );
