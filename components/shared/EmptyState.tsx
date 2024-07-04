14a6f79f2a0da42c5294e996000f9802a532d62a
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-04 13:16:46 -0600

    feat(EmptyState): implement the component empty state image

--- components/shared/EmptyState.tsx
+++ components/shared/EmptyState.tsx
@@ -1,5 +1,22 @@
-const EmptyState = () => {
-  return <div>EmptyState</div>;
+import { EmptyStateProps } from "@/types";
+import Image from "next/image";
+
+const EmptyState = ({
+  title,
+  search,
+  buttonLink,
+  buttonText,
+}: EmptyStateProps) => {
+  return (
+    <section className="flex-center size-full flex-col gap-3">
+      <Image
+        src="/icons/emptyState.svg"
+        alt="emptyState"
+        width={250}
+        height={250}
+      />
+    </section>
+  );
 };
 
 export default EmptyState;
