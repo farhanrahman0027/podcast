9a1202e5c2b044a0dcc71fad856dec372d56069b
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-04 13:23:17 -0600

    feat(EmptyState): implement the discover more podcasts button

--- components/shared/EmptyState.tsx
+++ components/shared/EmptyState.tsx
@@ -1,5 +1,7 @@
 import { EmptyStateProps } from "@/types";
 import Image from "next/image";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const EmptyState = ({
   title,
@@ -19,10 +21,27 @@ const EmptyState = ({
         <h1 className="text-16 text-center font-medium text-white-1">
           {title}
         </h1>
+
         {search && (
           <p className="text-16 text-center font-medium text-white-2">
             Try adjusting your search to find what you&apos;re looking for
           </p>
+        )}
+
+        {buttonLink && (
+          <Button className="bg-orange-1">
+            <Link href={buttonLink} className="gap-1 flex ">
+              <Image
+                src="/icons/discover.svg"
+                alt="discover"
+                width={20}
+                height={20}
+              />
+              <h1 className="text-16 font-extrabold text-white-1">
+                {buttonText}
+              </h1>
+            </Link>
+          </Button>
         )}
       </div>
     </section>
