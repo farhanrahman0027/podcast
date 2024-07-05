2f162f8c37a4fd94d6f9103a80271e517fbc396c
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-05 11:31:56 -0600

    feat(Carousel): implement the complete Carousel component

--- components/shared/Carousel.tsx
+++ components/shared/Carousel.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback } from "react";
 import { EmblaOptionsType, EmblaCarouselType } from "embla-carousel";
-import { useDotButton, DotButton } from "./EmblaCarouselDotButton";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
 import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
 import { CarouselProps } from "@/types";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import LoaderSpinner from "./LoaderSpinner";
 
 const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
   const router = useRouter();
@@ -32,32 +34,45 @@ const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
     fansLikeDetail &&
     fansLikeDetail?.filter((item: any) => item.totalPodcasts > 0);
 
-  console.log(slides);
+  if (!slides) return <LoaderSpinner />;
 
   return (
-    <section className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {slides.map((index) => (
-            <div className="embla__slide" key={index}>
-              <div className="embla__slide__number">{index + 1}</div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="embla__controls">
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={"embla__dot".concat(
-                index === selectedIndex ? " embla__dot--selected" : ""
-              )}
+    <section
+      className="flex w-full flex-col gap-4 overflow-hidden"
+      ref={emblaRef}
+    >
+      <div className="flex">
+        {slides.slice(0, 5).map((item) => (
+          <figure
+            key={item._id}
+            className="carousel_box"
+            onClick={() =>
+              router.push(`/podcasts/${item.podcast[0]?.podcastId}`)
+            }
+          >
+            <Image
+              src={item.imageUrl}
+              alt="card"
+              fill
+              className="absolute size-full rounded-xl border-none"
             />
-          ))}
-        </div>
+            <div className="glassmorphism-black relative z-10 flex flex-col rounded-b-xl p-4">
+              <h2 className="text-14 font-semibold text-white-1">
+                {item.podcast[0]?.podcastTitle}
+              </h2>
+              <p className="text-12 font-normal text-white-2">{item.name}</p>
+            </div>
+          </figure>
+        ))}
+      </div>
+      <div className="flex justify-center gap-2">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            onClick={() => onDotButtonClick(index)}
+            selected={index === selectedIndex}
+          />
+        ))}
       </div>
     </section>
   );
