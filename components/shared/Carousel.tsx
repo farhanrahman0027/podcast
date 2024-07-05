9cad62946f4a887ee84c5195379b16c219cdffcb
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-05 11:11:57 -0600

    feat(Carousel): implement the complete layout of the Carousel component

--- components/shared/Carousel.tsx
+++ components/shared/Carousel.tsx
@@ -1,5 +1,66 @@
-const Carousel = () => {
-  return <div>Carousel</div>;
+import React, { useCallback } from "react";
+import { EmblaOptionsType, EmblaCarouselType } from "embla-carousel";
+import { useDotButton, DotButton } from "./EmblaCarouselDotButton";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import { CarouselProps } from "@/types";
+import { useRouter } from "next/navigation";
+
+const EmblaCarousel = ({ fansLikeDetail }: CarouselProps) => {
+  const router = useRouter();
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (!autoplay || !("stopOnInteraction" in autoplay.options)) return;
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? (autoplay.reset as () => void)
+        : (autoplay.stop as () => void);
+
+    resetOrStop();
+  }, []);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
+    emblaApi,
+    onNavButtonClick
+  );
+
+  const slides =
+    fansLikeDetail &&
+    fansLikeDetail?.filter((item: any) => item.totalPodcasts > 0);
+
+  console.log(slides);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide" key={index}>
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default Carousel;
+export default EmblaCarousel;
