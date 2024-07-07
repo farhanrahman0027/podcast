ada37e7e7425b4d2e30ae88343a3913ccb248b0b
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-07 15:38:15 -0600

    feat(PodcastPlayer): implement the shadcn Progress component within the main component

--- components/shared/PodcastPlayer.tsx
+++ components/shared/PodcastPlayer.tsx
@@ -2,9 +2,13 @@
 
 import { cn } from "@/lib/utils";
 import { useAudio } from "@/providers/AudioProvider";
+import { Progress } from "../ui/progress";
+import { useState } from "react";
 
 const PodcastPlayer = () => {
   const { audio } = useAudio();
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
   return (
     <div
@@ -13,6 +17,11 @@ const PodcastPlayer = () => {
       })}
     >
       <h1 className="text-white-1 text-xl">{audio?.title}</h1>
+      <Progress
+        value={(currentTime / duration) * 100}
+        className="w-full"
+        max={duration}
+      />
     </div>
   );
 };
