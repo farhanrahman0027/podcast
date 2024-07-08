61a737e985a6a44366f3ac086615a001d4e3aa0a
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-07 20:41:45 -0600

    feat(MobileNav): implement the mobile nav to appear from the left side along as the project logo and name

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -6,20 +6,30 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
 
 const MobileNav = () => {
   return (
     <section>
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            alt="hamburger"
+            width={30}
+            height={30}
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-black-1">
+          <Link
+            href="/"
+            className="flex cursor-pointer items-center gap-1 pb-10 pl-4"
+          >
+            <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+            <h1 className="text-24 font-extrabold text-white-1">Podcastr</h1>
+          </Link>
         </SheetContent>
       </Sheet>
     </section>
