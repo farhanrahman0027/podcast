1ce49e38ee5c2a6eee2704e89e42a2609048de2e
saulkurosaki <saulkurosaki13@gmail.com>
2024-07-07 20:05:36 -0600

    feat(Discover): implement the discover page header title

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,7 +1,10 @@
 const Discover = () => {
   return (
-    <div>
-      <h1 className="text-20 font-bold text-white-1">Discover</h1>
+    <div className="flex flex-col gap-9">
+      Searchbar...
+      <div className="flex flex-col gap-9">
+        <h1 className="text-20 font-bold text-white-1">Discover</h1>
+      </div>
     </div>
   );
 };
