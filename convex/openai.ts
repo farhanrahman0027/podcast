12b63afaf96f7c7132bd4fce2368344b09fa8ae7
farhanur rahman <farhanrahman0027@gmail.com>
2025-02-12 00:51:58 +0530

    change the env

--- convex/openai.ts
+++ convex/openai.ts
@@ -5,7 +5,7 @@ import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
 });
 
 export const generateAudioAction = action({
